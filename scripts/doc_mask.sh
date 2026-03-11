5e44ae564e9d9d419a451cb571c07b63834d5b76
Jordan <zjzhang325@gmail.com>
2026-03-11 08:28:53 -0700

    save

---
+++ scripts/doc_mask.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+uv run torchrun --standalone --nproc_per_node=1 train.py \
+--wandb-entity zhuoranjzhang \
+--wandb-project tiny-lms \
+--wandb-run-name doc-mask\
+--intra-doc-mask \
+--n-kv-heads 12 \
+--n-layers 12 \
+--val-every 250 \
+--num-iterations 18865 \
+--batch-size 45 \
+--grad-accum-steps 11 \
+--learning-rate 6e-4 \
+--learning-rate-decay-frac 0.0 \
+--weight-decay 0.1 \
+--warmup-iters 700 \
+--wandb
