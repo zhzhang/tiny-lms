2000f0e19e9e187a3d3f17c56f2f97764ff80c57
Jordan <zjzhang325@gmail.com>
2026-04-25 11:27:57 -0700

    sdpa progress

--- sdpa_test.py
+++ sdpa_test.py
@@ -1,3 +1,4 @@
+import math
 from typing import Optional
 
 import torch
@@ -31,5 +32,36 @@ def my_sdpa(
     Returns:
         (N, ..., Hq, L, Ev)
     """
-    attn = torch.einsum("...qle,...qle", query, key)
-    scale = 
+    *q_shapes, q_seq, q_dim = query.shape
+    *k_shapes, k_seq, k_dim = key.shape
+    n_dims = len(q_shapes)
+    # if enable_gqa:
+    #     assert q_head >= k_head and q_head % k_head == 0
+    # else:
+    #     assert q_head == k_head
+
+    if attn_mask is None:
+        if is_causal:
+            attn_mask = torch.tril(q_seq, dtype=torch.bool)
+        else:
+            attn_mask = torch.ones(q_seq, k_seq, dtype=torch.bool)
+
+    if attn_mask.dtype == torch.bool:
+        mask_ninf = torch.full(attn_mask.shape, -float("inf"))
+        zeros = torch.zeros(attn_mask.shape)
+        attn_mask = torch.where(attn_mask, zeros, mask_ninf)
+
+    # Pad the mask to q x k if k > q
+    mask_q_size, mask_k_size = attn_mask.shape
+    assert mask_q_size == q_seq
+    if mask_k_size < k_seq:
+        pad = torch.full((mask_q_size, k_seq - mask_k_size), -float("inf"))
+        attn_mask = torch.cat((attn_mask, pad), -1)
+
+    print(attn_mask)
+    attn = torch.einsum("...le,...se->...ls", query, key)
+    attn = attn + attn_mask
+    attn /= math.sqrt(q_dim)
+    attn = attn.softmax(-1)
+    out = torch.einsum("...qv,...ve->...qe", attn, value)
+    return out
